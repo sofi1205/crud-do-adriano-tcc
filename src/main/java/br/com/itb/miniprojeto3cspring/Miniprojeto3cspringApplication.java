dadd66eb6b54d3abdd597a33f383ab7f48b7830b
Adriano Domingues <adriano.domingues@docente.fieb.edu.br>
2023-05-03 09:59:14 -0300

    Initial commit

---
+++ src/main/java/br/com/itb/miniprojeto3cspring/Miniprojeto3cspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojeto3cspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprojeto3cspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojeto3cspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itb/miniprojeto3cspring/Miniprojeto3cspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itb.miniprojeto3cspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojeto3cspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
